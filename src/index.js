5ae16cd85af69627a3fd422a7377d5074a923a6e
lomeat <lom3at@gmail.com>
2020-09-20 06:53:58 +0300

    feat: add webfontloader and global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import WebFont from "webfontloader";
+import { createGlobalStyle } from "styled-components";
 
 import { App } from "./App";
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
+
+WebFont.load({
+  google: {
+    families: ["Roboto"],
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
