32a12748e239fb47e4741479e1b3ce7351e64aba
lomeat <lom3at@gmail.com>
2020-09-20 07:34:41 +0300

    init: add nomalize

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import WebFont from "webfontloader";
 import { createGlobalStyle } from "styled-components";
+import "normalize.css";
 
 import { App } from "./App";
 
@@ -9,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   * {
     outline: none;
     box-sizing: border-box;
+    font-family: "Roboto:400,600,700,800", sans-serif;
   }
 `;
 
